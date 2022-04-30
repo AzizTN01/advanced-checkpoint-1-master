81972eb9ac9244dd83cc9f691d2b34b88c0a111b
AzizTN01 <azizbenrejen01@gmail.com>
2022-04-30 13:42:19 +0100

    commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AppContainer from "./AppContainer";
+import "./index.css";
+import store from "./store";
+import {Provider} from "react-redux";
+
+ReactDOM.render(
+  <Provider store={store}><AppContainer /></Provider>,
+  document.getElementById("root")
+);
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById("root")
+// );
